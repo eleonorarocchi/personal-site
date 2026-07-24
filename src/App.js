f7698a943e829cd6710b5d597970338b7aa01f11
Eleonora Rocchi <rocchi.eleonora@gmail.com>
2026-07-24 16:18:28 +0200

    feat: aggiunto analytics

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import Home from "./components/Home/Home.tsx";
 import Initiatives from "./components/Initiatives/Initiatives.tsx";
@@ -5,12 +6,27 @@ import './assets/css/bootstrap.min.css';
 import './assets/css/font-awesome.min.css';
 import './assets/css/style.css';
 import './assets/css/yellow.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Conferences from "./components/Conferences/Conferences.tsx";
+
+function RouteChangeTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <RouteChangeTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/iniziative" element={<Initiatives />} />
